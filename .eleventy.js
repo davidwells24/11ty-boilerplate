0bf46cf81c771b4bd15f786261198265942fc0b8
davidwells24 <136669947+davidwells24@users.noreply.github.com>
2025-02-03 04:19:44 +0000

    Added content to each html page with some css

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,11 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+  eleventyConfig.addShortcode("blog_post", function(image, description) {
+    return `<div class="blog_post">
+  <div class="blog_image">${image}</div>
+  <div class="description">${description}</div>
+</div>`;
+  });
 };
